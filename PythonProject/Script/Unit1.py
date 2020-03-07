24c244b97d54551e746af2d8eef945951cdaa0f6
Tanmaya <Tanmayageit@gmail.com>
2020-03-07 22:08:45 +0530

    TestComplete first project check in

---
+++ PythonProject/Script/Unit1.py
@@ -0,0 +1,13 @@
+﻿from Unit2 import *
+
+
+def test():
+  Log.Message("hello testing")
+  Browsers.Item[btChrome].Run('https://www.facebook.com/')
+  chrome = Sys.Browser('Chrome').Page("https://www.facebook.com/")
+  chrome.FindChildByXPath('//*[@id="u_0_m"]').SetText('Tanmaya')
+  
+  
+def test2():
+  Log.Message("Hello Automation testing")
+  library()
---
+++ PythonProject/Script/Unit2.py
@@ -0,0 +1,2 @@
+﻿def library():
+  Log.Message("Imported")
